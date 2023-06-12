58d9410986dcfbac012eb1efe9f35d31e4f62402
Mehrob-Sharipov <mehrob_sharipov@icloud.com>
2023-06-12 06:39:25 +0500

    second commit

---
+++ GitDemo/Model.swift
@@ -0,0 +1,11 @@
+//
+//  MOdel.swift
+//  GitDemo
+//
+//  Created by Sharipov Mehrob on 12.06.2023.
+//
+
+import Foundation
+class Model{
+    let user: String = ""
+}
---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Sharipov Mehrob on 31.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let mylable = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mylable.text = "firstCommit"
+        mylable.text = "secondCommit"
+    }
+
+
+}
+
